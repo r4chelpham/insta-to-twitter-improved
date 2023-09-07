aa9832423cf2af7d7f0cbe4b17e9c58c900ac3de
r4chelpham <r4chelpham@gmail.com>
2023-09-07 19:08:04 +0100

    first commit

---
+++ get_insta_posts.py
@@ -0,0 +1,54 @@
+import instaloader
+from datetime import datetime
+import os
+
+class GetInstaPosts():
+
+    def __init__(self):
+        self.L = instaloader.Instaloader()
+        self.L.save_metadata = False
+        self.L.post_metadata_txt_pattern = ''
+
+    '''
+    Gets all the posts on a specific date from a given user's profile.
+
+    Parameters:
+    username(str): The username of the profile to get the posts from.
+    date(datetime): The datetime object of the date of when to get the posts from.
+
+    Returns:
+    list: A list of elements representing in the following order - caption of post, a list of the media posted
+    '''
+
+    def get_media(self, username, date):
+        profile = instaloader.Profile.from_username(self.L.context, username)
+        posts = profile.get_posts()
+
+        new_posts = [post for post in posts if datetime.strftime(post.date, '%Y-%m-%d') == datetime.strftime(date, '%Y-%m-%d')]
+
+        list_posts = []
+
+        for post in new_posts:
+            date = datetime.strftime(post.date, '%Y-%m-%d')
+            folder_name = r"C:\Users\r4che\Downloads\tweet-poster-alt\posts\New Post " + date
+            if not os.path.exists(folder_name):
+                os.mkdir(folder_name)
+            self.L.dirname_pattern = folder_name
+
+            self.L.download_post(post, target=folder_name)
+
+            data = [post.caption]
+
+            media = []
+
+            if post.typename == 'GraphSidecar':
+                for i in range(post.mediacount):
+                    file_name = str(post.date_utc).replace(":", "-").replace(" ", "_") + '_UTC_' + f'{i+1}' + '.jpg'
+                    media.append(file_name)
+            else:
+                media.append(self.L.format_filename(post, target=profile.username) + '.jpg')
+            data.append(media)
+            list_posts.append(data)
+
+        return list_posts
+    
---
+++ main.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from get_insta_posts import GetInstaPosts
+import os
+from datetime import datetime
+import tweepy
+
+load_dotenv()
+
+BEARER_TOKEN = os.getenv('bearer_token')
+API_KEY = os.getenv('api_key')
+API_SECRET = os.getenv('api_secret')
+ACCESS_TOKEN = os.getenv('access_token')
+ACCESS_TOKEN_SECRET = os.getenv('access_token_secret')
+
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+insta = GetInstaPosts()
+
+date = datetime(2023, 8, 28)
+
+file_destination = r'C:\Users\r4che\Downloads\tweet-poster-alt\posts\New Post ' + datetime.strftime(date, '%Y-%m-%d') + "\\"
+print(file_destination)
+
+posts_data = insta.get_media('kclwistem', date)
+
+print(posts_data)
+
+for post in posts_data:
+    media_ids = []
+    caption = '' if not post[0] else post[0]
+
+    for media in post[1]:
+        media_path = file_destination + media
+        file_name = file_destination + media
+        m = api.media_upload(file_name)
+        media_id = m.media_id
+        media_ids.append(media_id)
+        print(media_ids)
+
+    client.create_tweet(text = caption, media_ids= media_ids, user_auth = True)
+
